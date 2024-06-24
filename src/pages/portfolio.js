b1d21d2dbf56804d9aa5cd7f982c88efd973c652
Daniel Lopez <dlopez807@gmail.com>
2024-06-24 11:56:39 -0700

    update react and next

--- src/pages/portfolio.js
+++ src/pages/portfolio.js
@@ -12,7 +12,7 @@ const Portfolio = ({ portfolio }) => (
         {portfolio.map(({ url, text, description }) => (
           <li
             key={url}
-            className='rounded-lg bg-white text-cobalt shadow md:hover:bg-blue-100'
+            className='relative rounded-lg bg-white text-cobalt shadow md:hover:bg-blue-100'
           >
             <a
               className='block h-full p-4 md:text-2xl'
@@ -22,25 +22,26 @@ const Portfolio = ({ portfolio }) => (
             >
               <span className='flex items-center space-x-2 font-semibold leading-tight'>
                 <span className='flex-grow'>{text}</span>
-                <Link href={`/resume#${text}`}>
-                  <a>
-                    <svg
-                      xmlns='http://www.w3.org/2000/svg'
-                      viewBox='0 0 20 20'
-                      fill='currentColor'
-                      className='h-6 w-6 fill-current hover:text-cb-pink'
-                    >
-                      <path
-                        fillRule='evenodd'
-                        d='M18 10a8 8 0 11-16 0 8 8 0 0116 0zm-7-4a1 1 0 11-2 0 1 1 0 012 0zM9 9a1 1 0 000 2v3a1 1 0 001 1h1a1 1 0 100-2v-3a1 1 0 00-1-1H9z'
-                        clipRule='evenodd'
-                      />
-                    </svg>
-                  </a>
-                </Link>
               </span>
               <p>{description}</p>
             </a>
+            <Link
+              href={`/resume#${text}`}
+              className='absolute top-4 right-4 text-cobalt'
+            >
+              <svg
+                xmlns='http://www.w3.org/2000/svg'
+                viewBox='0 0 20 20'
+                fill='currentColor'
+                className='h-6 w-6 fill-current hover:text-cb-pink'
+              >
+                <path
+                  fillRule='evenodd'
+                  d='M18 10a8 8 0 11-16 0 8 8 0 0116 0zm-7-4a1 1 0 11-2 0 1 1 0 012 0zM9 9a1 1 0 000 2v3a1 1 0 001 1h1a1 1 0 100-2v-3a1 1 0 00-1-1H9z'
+                  clipRule='evenodd'
+                />
+              </svg>
+            </Link>
           </li>
         ))}
       </ul>
